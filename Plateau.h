7cd04b5c7666b08c873d90bfbab77fd8d194b7a3
O7lo <80862226+O7lo@users.noreply.github.com>
2021-12-10 15:07:43 -0500

    ajouté SalleDeDépart_ au plateau

--- Plateau.h
+++ Plateau.h
@@ -9,6 +9,9 @@ public:
 	Plateau() = default;
 	void ajouterSalle(std::unique_ptr<Salle> salle);
 	void connecterVoisins(Salle* salle1, int direction, Salle* salle2);
+	void setSalleDeDepart(Salle* salle) { salleDeDepart_ = salle; }
+	Salle* getSalleDeDepart() { return salleDeDepart_; }
 private:
 	std::vector<std::unique_ptr<Salle>> salles_;
+	Salle* salleDeDepart_;
 };

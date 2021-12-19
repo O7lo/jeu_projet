69cba88a95de7050ce3abd7437e9dbb6ffc09bdc
Arthur Panoyan <arthur.panoyan@polymtl.ca>
2021-12-19 12:13:29 -0500

    class objet ajouter et method pour ajouer objet dans salle, pas tester

--- Salle.cpp
+++ Salle.cpp
@@ -4,3 +4,8 @@ void Salle::connecterVoisin(Salle* voisin, int direction){
 	voisins_[direction] = voisin;
 	std::cout << "voisin connecté: " << voisins_[direction]->getNom()<<" "<< nomsDirections[direction] <<"\t";
 }
+
+void Salle::ajouterObjet(Objets& objet) {
+	objets_.insert({ objet.getNom(), std::make_unique<Objets>(objet) });
+	std::cout << "objet ajoutee dans la salle: " << objets_[objet.getNom()]->getNom() << "\t";
+}
